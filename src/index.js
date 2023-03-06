2eb8f34d50d9d1bedd4a4cede27b866c897d48bc
desmond58 <chiping5858@gmail.com>
2023-03-07 00:14:50 +0800

    updated reactjs code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import dashboard from './dashboard';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<dashboard />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
