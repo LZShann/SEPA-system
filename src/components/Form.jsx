06d47d85a517aa47512058a073112bdb3ee9ce13
desmond58 <chiping5858@gmail.com>
2023-04-07 15:55:35 +0800

    adding component/form and modify page/import data

---
+++ src/components/Form.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // TODO: Submit the form data to the database
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="phone">Phone</label>
+        <input
+          type="tel"
+          className="form-control"
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea
+          className="form-control"
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/pages/Import_Dataset.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../components/Form';
+
+const Import_Dataset = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Import Dataset</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default Import_Dataset;
