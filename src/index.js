5cca77f509ecf48b61032437869ad640bd0d553f
LZShann <92285116+LZShann@users.noreply.github.com>
2023-04-10 15:04:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import Login from './Login';
+import Register from './Register';
 import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
     <React.StrictMode>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/Login" element={(<Login />)} />
+            <Route path="/Register" element={(<Register />)} />
+          </Routes>
+        </BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
