10d04b7f7456fa5b868436de9cea78bedb625185
desmond58 <chiping5858@gmail.com>
2023-04-30 01:27:06 +0800

    achieved and remove unwanted api

--- src/archieve/contexts/ContextProvider.js
+++ src/archieve/contexts/ContextProvider.js
@@ -4,6 +4,7 @@ const StateContext = createContext();
 
 const initialState = {
   chat: false,
+  cart: false,
   userProfile: false,
   notification: false,
 };
@@ -11,8 +12,15 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(undefined);
   const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+  };
 
   const setColor = (color) => {
     setCurrentColor(color);
@@ -23,7 +31,7 @@ export const ContextProvider = ({ children }) => {
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <StateContext.Provider value={{ currentColor, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setColor }}>
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
       {children}
     </StateContext.Provider>
   );
