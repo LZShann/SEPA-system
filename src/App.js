f4f6a88fafb1cd6eb43b5664480269fc5c65a8a6
LZShann <92285116+LZShann@users.noreply.github.com>
2023-05-04 13:11:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import { Login, SignUp, Homepage } from './pages';
 import { Statistic, Import_Dataset, Interview, Task, Account} from './pages';
 import './App.css';
 
-<<<<<<< Updated upstream
 import { useStateContext } from './contexts/ContextProvider';
 
 
 
-=======
->>>>>>> Stashed changes
 const App = () => {
 
   const [token, setToken] = useState(false)
@@ -28,56 +25,6 @@ const App = () => {
 
   return (
     <div>
-<<<<<<< Updated upstream
-      <BrowserRouter>
-        
-        <div className="flex relative">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent
-              content="Settings"
-              position="Top"
-            >
-            </TooltipComponent>
-          </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar bg-white ">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0">
-              <Sidebar />
-            </div>
-          )}
-          <div
-            className={
-              activeMenu
-                ? 'bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg w-full min-h-screen flex-2 '
-            }
-          >
-            <div className="fixed md:static bg-main-bg navbar w-full ">
-              <Navbar />
-            </div>
-            <div>
-              <Routes>
-                {/* dashboard  */}
-                <Route path="/" element={(<Statistic />)} />
-                <Route path="/Statistic" element={(<Statistic />)} />
-
-                {/* pages  */}
-                <Route path="/Task" element={(<Task />)} />
-                <Route path="/Import_Dataset" element={(<Import_Dataset />)} />
-                <Route path="/Interview" element={(<Interview />)} />
-
-                {/* Admin */}
-                <Route path="/Account" element={(<Account />)} />
-              </Routes>
-            </div>
-            <Footer/>
-          </div>
-        </div>
-      </BrowserRouter>
-=======
       <Routes>
         <Route path={'/'} element={<Login setToken={setToken}/>}/>
         <Route path={'/SignUp'} element={<SignUp />}/>
@@ -90,7 +37,6 @@ const App = () => {
               <Route path="Account" element={<Account />} />
         </Route>:''}
       </Routes>
->>>>>>> Stashed changes
     </div>
   )
 }
