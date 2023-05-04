8403600fe653a4bf2e4071c591f937008affd5e4
desmond58 <chiping5858@gmail.com>
2023-05-04 13:00:33 +0800

    added import excel button

--- src/pages/Import_Dataset.jsx
+++ src/pages/Import_Dataset.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import {
-  Header, Forms, TopProduct, SalesByMonth, SalesByRegion, SalesDataEntry, CustomerDataEntry, TransporterDataEntry,
-  UnitPriceDataEntry
+  Header, SalesDataEntry, CustomerDataEntry, TransporterDataEntry, UnitPriceDataEntry
 } from '../components';
-import { Form } from 'react-router-dom';
 
 const Import_Dataset = () => {
-  const [selectedForm, setSelectedForm] = useState('TopProduct');
+  const [selectedForm, setSelectedForm] = useState(''); //useState() empty because set default no form selected
 
   const handleSelectChange = (event) => {
     setSelectedForm(event.target.value);
@@ -24,10 +22,26 @@ const Import_Dataset = () => {
     cursor: 'pointer',
     fontSize: '1rem',
   };
-
+  const buttonStyle = {
+    backgroundColor: 'rgb(3,201,215)',
+    color: 'white',
+    marginTop: '10px',    
+    marginBottom: '10px',
+    display: 'block',
+    width: '30%',
+    height: '40px',
+    borderRadius: '5px',
+    padding: '0 20px',
+    cursor: 'pointer',
+  };
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Import_Dataset" />
+      <div>
+        <p style={{ fontWeight: 'bold' }}>Import Excel</p>
+        <button type="button" style={buttonStyle}>
+        Press Import Csv</button>
+      </div>
       <div>
         <label htmlFor="form-select" style={{ fontWeight: 'bold' }}>Select a form:</label>
         <select id="form-select" value={selectedForm} onChange={handleSelectChange} style={selectStyle}>
@@ -44,7 +58,6 @@ const Import_Dataset = () => {
       {selectedForm === 'TransporterDataEntry' && <TransporterDataEntry />}
       {selectedForm === 'UnitPriceDataEntry' && <UnitPriceDataEntry />}
       {/* {selectedForm === 'TopProduct' && <TopProduct/>} */}
-      <br /><Forms />
     </div>
 
   );
