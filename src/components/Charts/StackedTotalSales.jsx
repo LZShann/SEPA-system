20a9b4beaf0cec9c345ee0a04c14de656fb25d74
lijinliew <lijinliew8669@gmail.com>
2023-05-31 20:51:50 +0800

    modified StackedTitalSales (filename not changed)

--- src/components/Charts/StackedTotalSales.jsx
+++ src/components/Charts/StackedTotalSales.jsx
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from 'react';
-// Stacked Bar Chart Component From Syncfusion
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, ColumnSeries, Tooltip, NumericAxis } from '@syncfusion/ej2-react-charts';
 import { useStateContext } from '../../contexts/ContextProvider';
 
-// Database
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = 'https://aehwgrirrnhmatqmqcsa.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaHdncmlycm5obWF0cW1xY3NhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDg2NTg4MywiZXhwIjoxOTk2NDQxODgzfQ.DeXxoWY65kzpbvdxME16mAHj2KGMwDRg_jEGgUIxKc0';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Stacked Bar Chart Data Source passing and X,Y Axis Name
-var stackedCustomSeries = [
+var barCustomSeries = [
   {
     dataSource: [0],
     xName: 'x',
     yName: 'y',
-    name: 'Revenue',
-    type: 'StackingColumn',
-  },
-  {
-    dataSource: [1],
-    xName: 'x',
-    yName: 'y',
-    name: 'Pencentage of Revenue ÷100',
-    type: 'StackingColumn',
+    name: 'Sales',
+    type: 'Column',
   },
 ];
 
-// Stacked Bar Chart Pattern/Style
-var stackedPrimaryXAxis = {
+var barPrimaryXAxis = {
   majorGridLines: { width: 0 },
   minorGridLines: { width: 0 },
   majorTickLines: { width: 0 },
@@ -38,7 +27,8 @@ var stackedPrimaryXAxis = {
   labelIntersectAction: 'Rotate45',
   valueType: 'Category',
 };
-var stackedPrimaryYAxis = {
+
+var barPrimaryYAxis = {
   lineStyle: { width: 0 },
   minimum: 0,
   maximum: 50000,
@@ -50,95 +40,69 @@ var stackedPrimaryYAxis = {
   labelFormat: '{value}',
 };
 
-
-var StackedTotalSales = ({ width, height }) => {
+var BarTotalSales = ({ width, height }) => {
   const { currentMode } = useStateContext();
-  // Data Setter
-  const [stackedBarData, setStackedBarData2] = useState(null);
-  // Error Setter *Used when error occur
-  const [fetchError, setFetchError2] = useState(null);
-  // Key Prop
+  const [barData, setBarData] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
   const [chartKey, setChartKey] = useState(0);
 
   useEffect(() => {
-    // Function to fetch data from database
-    const fetchStackedBarData2 = async () => {
-      let { data: stackedBarData, error } = await supabase
-        .from('sales_data_entry') // Table name
-        .select('*, product:products_data_entry(*)') // Select all data from sales_data_entry and foreign key - product from products_data_entry & select all
-        .eq('sales_man_name', 'TYS'); // equal to sales man name LWH *Can change the LWH to other sales man name
+    const fetchBarData = async () => {
+      let { data: barData, error } = await supabase
+        .from('sales_data_entry')
+        .select('*, product:products_data_entry(*)')
+        .eq('sales_man_name', 'TYS');
 
-      if (error) { // for error checking only 
-        setFetchError2('Could not fetch data from sales_data_entry');
-        setStackedBarData2(null);
+      if (error) {
+        setFetchError('Could not fetch data from sales_data_entry');
+        setBarData(null);
         console.log('error', error);
       }
 
-      if (stackedBarData) {
-        setFetchError2(null);
-        setStackedBarData2(stackedBarData); // if data found when be here. Use stackedBarData to do the data manipulation
+      if (barData) {
+        setFetchError(null);
+        setBarData(barData);
 
-        var newStackedChartData = []; // used to find revenue
-        var newStackedChartData2 = []; // used to find revenue percentage
-        var totalRevenue = 0; // used to find total revenue
+        var newBarChartData = [];
+        var totalRevenue = 0;
 
-        stackedBarData.forEach((item) => {
-          const revenue = item['quantity'] * item['product']['unit_price']; // revenue formula
-          const product_name = item['product']['product_name']; // product name
+        barData.forEach((item) => {
+          const revenue = item['quantity'] * item['product']['unit_price'];
+          const product_name = item['product']['product_name'];
 
-          totalRevenue += revenue; // total revenue
+          totalRevenue += revenue;
 
-          // find the product name in the array and add the revenue to the product name
-          const productIndex = newStackedChartData.findIndex((item) => item['x'] === product_name);
-          // if product name not found in the array, push the product name and revenue to the array
+          const productIndex = newBarChartData.findIndex((item) => item['x'] === product_name);
+
           if (productIndex === -1) {
-            newStackedChartData.push({
+            newBarChartData.push({
               x: product_name,
               y: revenue,
             });
           } else {
-            // if product name found in the array, add the revenue to the product name
-            newStackedChartData[productIndex]['y'] += revenue;
+            newBarChartData[productIndex]['y'] += revenue;
           }
         });
-        // reuse the newStackedChartData to find the percentage
-        newStackedChartData.forEach((item) => {
-          const revenue = item['y'];
-          // calculate the percentage
-          const percentage = (revenue / totalRevenue) * 10000;
-          const percentageRounded = Number(percentage.toFixed(2)); // limit the percentage to 2 decimal places
 
-          // push the product name and percentage to the array
-          newStackedChartData2.push({
-            x: item['x'],
-            y: percentageRounded,
-          });
-        });
+        newBarChartData.sort((a, b) => b['y'] - a['y']);
 
-        // Sort the array from highest to lowest
-        newStackedChartData.sort((a, b) => b['y'] - a['y']);
-        newStackedChartData2.sort((a, b) => b['y'] - a['y']);
+        barCustomSeries[0]['dataSource'] = newBarChartData;
 
-        // set the newStackedChartData and newStackedChartData2 to the dataSource
-        stackedCustomSeries[0]['dataSource'] = newStackedChartData;
-        stackedCustomSeries[1]['dataSource'] = newStackedChartData2;
+        console.log(barCustomSeries[0]['dataSource']);
 
-        // check the output for revenue and percentage of revenue, it same as 'console.log(newStackedChartData);'
-        console.log(stackedCustomSeries[0]['dataSource']);
-        console.log(stackedCustomSeries[1]['dataSource']);
+        barCustomSeries[0]['dataSource'] = [{ x: 'Total Sales', y: totalRevenue }];
 
-        // set the chartKey to re-render the chart
+        barPrimaryYAxis.maximum = totalRevenue;
+        barPrimaryYAxis.interval = totalRevenue / 5;
+
         setChartKey(prevKey => prevKey + 1);
 
-        // HTML update for title, highest revenue, lowest revenue and highest revenue percentage
         var titleElement = document.getElementById('totalSalesTitle');
         titleElement.innerHTML = 'Total Sales 2021 (Sales by Salesmen)';
 
-        // Allocate the first element as the highest and lowest revenue
-        var highestRevenue = newStackedChartData[0]['y'];
+        var highestRevenue = newBarChartData[0]['y'];
 
-        // Use loop if the next value higheer or lower than the previous value, replace the value
-        newStackedChartData.forEach((item) => {
+        newBarChartData.forEach((item) => {
           if (item['y'] > highestRevenue) {
             highestRevenue = item['y'];
           }
@@ -149,15 +113,15 @@ var StackedTotalSales = ({ width, height }) => {
       }
     };
 
-    fetchStackedBarData2();
+    fetchBarData();
   }, []);
 
   return (
     <ChartComponent
-      id="stackedTotalSales"
+      id="barTotalSales"
       key={chartKey}
-      primaryXAxis={stackedPrimaryXAxis}
-      primaryYAxis={stackedPrimaryYAxis}
+      primaryXAxis={barPrimaryXAxis}
+      primaryYAxis={barPrimaryYAxis}
       width={width}
       height={height}
       chartArea={{ border: { width: 0 } }}
@@ -165,12 +129,12 @@ var StackedTotalSales = ({ width, height }) => {
       background={currentMode === 'Dark' ? '#33373E' : '#fff'}
       legendSettings={{ background: 'white' }}
     >
-      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <Inject services={[ColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+        {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
     </ChartComponent>
   );
 };
 
-export default StackedTotalSales;
+export default BarTotalSales;
