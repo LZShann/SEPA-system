ab5afdc6caae12469216880366097101b210d027
lijinliew <lijinliew8669@gmail.com>
2023-06-04 00:02:28 +0800

    set chart visibility

--- src/components/Charts/BarSalesByRegion2.jsx
+++ src/components/Charts/BarSalesByRegion2.jsx
@@ -8,138 +8,138 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 var barCustomSeries = [
-  {
-    dataSource: [0],
-    xName: 'order_date',
-    yName: 'percentage',
-    name: 'Sales',
-    type: 'Column',
-  },
+    {
+        dataSource: [0],
+        xName: 'order_date',
+        yName: 'percentage',
+        name: 'Sales',
+        type: 'Column',
+    },
 ];
 
 var barPrimaryXAxis = {
-  majorGridLines: { width: 0 },
-  minorGridLines: { width: 0 },
-  majorTickLines: { width: 0 },
-  minorTickLines: { width: 0 },
-  interval: 1,
-  valueType: 'Category',
-  labelIntersectAction: 'Rotate45',
-  labelPlacement: 'OnTicks',
+    majorGridLines: { width: 0 },
+    minorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    minorTickLines: { width: 0 },
+    interval: 1,
+    valueType: 'Category',
+    labelIntersectAction: 'Rotate45',
+    labelPlacement: 'OnTicks',
 };
 
 var barPrimaryYAxis = {
-  lineStyle: { width: 0 },
-  minimum: 0,
-  maximum: 100, // Updated maximum to 100 for percentage
-  interval: 20, // Updated interval to 20 for percentage
-  majorTickLines: { width: 0 },
-  majorGridLines: { width: 1 },
-  minorGridLines: { width: 1 },
-  minorTickLines: { width: 0 },
-  labelFormat: '{value}%', // Updated label format to display percentage
+    lineStyle: { width: 0 },
+    minimum: 0,
+    maximum: 100, // Updated maximum to 100 for percentage
+    interval: 20, // Updated interval to 20 for percentage
+    majorTickLines: { width: 0 },
+    majorGridLines: { width: 1 },
+    minorGridLines: { width: 1 },
+    minorTickLines: { width: 0 },
+    labelFormat: '{value}%', // Updated label format to display percentage
 };
 
 var BarSalesByRegion2 = ({ width, height }) => {
-  const { currentMode } = useStateContext();
-  const [barData, setBarData] = useState(null);
-  const [fetchError, setFetchError] = useState(null);
-  const [chartKey, setChartKey] = useState(0);
+    const { currentMode } = useStateContext();
+    const [barData, setBarData] = useState(null);
+    const [fetchError, setFetchError] = useState(null);
+    const [chartKey, setChartKey] = useState(0);
 
-  useEffect(() => {
-    const fetchBarData = async () => {
-      let { data: barData, error } = await supabase
-        .from('sales_data_entry')
-        .select('*, product:products_data_entry(*)')
-        .eq('sales_man_name', 'TYS');
+    useEffect(() => {
+        const fetchBarData = async () => {
+            let { data: barData, error } = await supabase
+                .from('sales_data_entry')
+                .select('*, product:products_data_entry(*)')
+                .eq('sales_man_name', 'TYS');
 
-      if (error) {
-        setFetchError('Could not fetch data from sales_data_entry');
-        setBarData(null);
-        console.log('error', error);
-      }
+            if (error) {
+                setFetchError('Could not fetch data from sales_data_entry');
+                setBarData(null);
+                console.log('error', error);
+            }
 
-      if (barData) {
-        setFetchError(null);
-        setBarData(barData);
+            if (barData) {
+                setFetchError(null);
+                setBarData(barData);
 
-        var newBarChartData = [];
-        var totalRevenueByMonth = {};
+                var newBarChartData = [];
+                var totalRevenueByMonth = {};
 
-        barData.forEach((item) => {
-          const revenue = item['quantity'] * item['product']['unit_price'];
-          const orderDate = new Date(item['order_date']);
-          const month = orderDate.getMonth(); // Get the month value (0-11)
+                barData.forEach((item) => {
+                    const revenue = item['quantity'] * item['product']['unit_price'];
+                    const orderDate = new Date(item['order_date']);
+                    const month = orderDate.getMonth(); // Get the month value (0-11)
 
-          totalRevenueByMonth[month] = (totalRevenueByMonth[month] || 0) + revenue;
-        });
+                    totalRevenueByMonth[month] = (totalRevenueByMonth[month] || 0) + revenue;
+                });
 
-        const totalRevenue = Object.values(totalRevenueByMonth).reduce((sum, revenue) => sum + revenue, 0);
+                const totalRevenue = Object.values(totalRevenueByMonth).reduce((sum, revenue) => sum + revenue, 0);
 
-        Object.keys(totalRevenueByMonth).forEach((month) => {
-            const revenue = totalRevenueByMonth[month];
-            const percentage = ((revenue / totalRevenue) * 100).toFixed(2); // Round to 2 decimal points
-            const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                Object.keys(totalRevenueByMonth).forEach((month) => {
+                    const revenue = totalRevenueByMonth[month];
+                    const percentage = ((revenue / totalRevenue) * 100).toFixed(2); // Round to 2 decimal points
+                    const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-            newBarChartData.push({
-              order_date: monthLabels[month],
-              percentage: parseFloat(percentage), // Convert the percentage back to a floating-point number
-            });
-          });
-          
+                    newBarChartData.push({
+                        order_date: monthLabels[month],
+                        percentage: parseFloat(percentage), // Convert the percentage back to a floating-point number
+                    });
+                });
 
-        barCustomSeries[0]['dataSource'] = newBarChartData;
 
-        setChartKey((prevKey) => prevKey + 1);
+                barCustomSeries[0]['dataSource'] = newBarChartData;
 
-        var titleElement = document.getElementById('SalesByRegion2Title');
-        titleElement.innerHTML = 'Sales By Region 2 (Grand Total %)';
+                setChartKey((prevKey) => prevKey + 1);
 
-        var highestPercentage = newBarChartData[0]['percentage'];
-        var lowestPercentage = newBarChartData[0]['percentage'];
-        
-        newBarChartData.forEach((item) => {
-          if (item['percentage'] > highestPercentage) {
-            highestPercentage = item['percentage'];
-          }
-        
-          if (item['percentage'] < lowestPercentage) {
-            lowestPercentage = item['percentage'];
-          }
-        });
+                var titleElement = document.getElementById('SalesByRegion2Title');
+                titleElement.innerHTML = 'Sales By Region 2 (Grand Total %)';
 
-        var highestPercentageElement = document.getElementById('topSalesByRegion2');
-        highestPercentageElement.innerHTML = highestPercentage.toFixed(2) + '%';
+                var highestPercentage = newBarChartData[0]['percentage'];
+                var lowestPercentage = newBarChartData[0]['percentage'];
 
-        var lowestPercentageElement = document.getElementById('lowestSalesByRegion2');
-        lowestPercentageElement.innerHTML = lowestPercentage.toFixed(2) + '%';
-      }
-    };
+                newBarChartData.forEach((item) => {
+                    if (item['percentage'] > highestPercentage) {
+                        highestPercentage = item['percentage'];
+                    }
 
-    fetchBarData();
-  }, []);
+                    if (item['percentage'] < lowestPercentage) {
+                        lowestPercentage = item['percentage'];
+                    }
+                });
 
-  return (
-    <ChartComponent
-      id="barSalesByRegion2"
-      key={chartKey}
-      primaryXAxis={barPrimaryXAxis}
-      primaryYAxis={barPrimaryYAxis}
-      width={width}
-      height={height}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      legendSettings={{ background: 'white' }}
-    >
-      <Inject services={[ColumnSeries, Category, Legend, Tooltip, DateTime]} />
-      <SeriesCollectionDirective>
-        {barCustomSeries.map((item, index) => (
-          <SeriesDirective key={index} {...item} />
-        ))}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  );
+                var highestPercentageElement = document.getElementById('topSalesByRegion2');
+                highestPercentageElement.innerHTML = highestPercentage.toFixed(2) + '%';
+
+                var lowestPercentageElement = document.getElementById('lowestSalesByRegion2');
+                lowestPercentageElement.innerHTML = lowestPercentage.toFixed(2) + '%';
+            }
+        };
+
+        fetchBarData();
+    }, []);
+
+    return (
+        <ChartComponent
+            id="barSalesByRegion2"
+            key={chartKey}
+            primaryXAxis={barPrimaryXAxis}
+            primaryYAxis={barPrimaryYAxis}
+            width={width}
+            height={height}
+            chartArea={{ border: { width: 0 } }}
+            tooltip={{ enable: true }}
+            background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+            legendSettings={{ background: 'white' }}
+        >
+            <Inject services={[ColumnSeries, Category, Legend, Tooltip, DateTime]} />
+            <SeriesCollectionDirective>
+                {barCustomSeries.map((item, index) => (
+                    <SeriesDirective key={index} {...item} />
+                ))}
+            </SeriesCollectionDirective>
+        </ChartComponent>
+    );
 };
 
 export default BarSalesByRegion2;
--- src/components/Charts/BarTop10Customers.jsx
+++ src/components/Charts/BarTop10Customers.jsx
@@ -8,139 +8,139 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 var barCustomSeries = [
-  {
-    dataSource: [0],
-    xName: 'x',
-    yName: 'y',
-    name: 'Sales',
-    type: 'Column',
-  },
+    {
+        dataSource: [0],
+        xName: 'x',
+        yName: 'y',
+        name: 'Sales',
+        type: 'Column',
+    },
 ];
 
 var barPrimaryXAxis = {
-  majorGridLines: { width: 0 },
-  minorGridLines: { width: 0 },
-  majorTickLines: { width: 0 },
-  minorTickLines: { width: 0 },
-  interval: 1,
-  lineStyle: { width: 0 },
-  labelIntersectAction: 'Rotate45',
-  valueType: 'Category',
+    majorGridLines: { width: 0 },
+    minorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    minorTickLines: { width: 0 },
+    interval: 1,
+    lineStyle: { width: 0 },
+    labelIntersectAction: 'Rotate45',
+    valueType: 'Category',
 };
 
 var barPrimaryYAxis = {
-  lineStyle: { width: 0 },
-  minimum: 0,
-  maximum: 20000,
-  interval: 5000,
-  majorTickLines: { width: 0 },
-  majorGridLines: { width: 1 },
-  minorGridLines: { width: 1 },
-  minorTickLines: { width: 0 },
-  labelFormat: '{value}',
+    lineStyle: { width: 0 },
+    minimum: 0,
+    maximum: 20000,
+    interval: 5000,
+    majorTickLines: { width: 0 },
+    majorGridLines: { width: 1 },
+    minorGridLines: { width: 1 },
+    minorTickLines: { width: 0 },
+    labelFormat: '{value}',
 };
 
 var BarTop10Customers = ({ width, height }) => {
-  const { currentMode } = useStateContext();
-  const [barData, setBarData] = useState(null);
-  const [fetchError, setFetchError] = useState(null);
-  const [chartKey, setChartKey] = useState(0);
+    const { currentMode } = useStateContext();
+    const [barData, setBarData] = useState(null);
+    const [fetchError, setFetchError] = useState(null);
+    const [chartKey, setChartKey] = useState(0);
 
-  useEffect(() => {
-    const fetchBarData = async () => {
-      let { data: barData, error } = await supabase
-        .from('sales_data_entry')
-        .select('*, product:products_data_entry(*)')
-        .eq('sales_man_name', 'TYS');
+    useEffect(() => {
+        const fetchBarData = async () => {
+            let { data: barData, error } = await supabase
+                .from('sales_data_entry')
+                .select('*, product:products_data_entry(*)')
+                .eq('sales_man_name', 'TYS');
 
-      if (error) {
-        setFetchError('Could not fetch data from sales_data_entry');
-        setBarData(null);
-        console.log('error', error);
-      }
+            if (error) {
+                setFetchError('Could not fetch data from sales_data_entry');
+                setBarData(null);
+                console.log('error', error);
+            }
 
-      if (barData) {
-        setFetchError(null);
-        setBarData(barData);
+            if (barData) {
+                setFetchError(null);
+                setBarData(barData);
 
-        var newBarChartData = [];
-        var totalRevenue = 0;
+                var newBarChartData = [];
+                var totalRevenue = 0;
 
-        barData.forEach((item) => {
-          const revenue = item['quantity'] * item['product']['unit_price'];
-          const customerId = item['customer_id'];
+                barData.forEach((item) => {
+                    const revenue = item['quantity'] * item['product']['unit_price'];
+                    const customerId = item['customer_id'];
 
-          totalRevenue += revenue;
+                    totalRevenue += revenue;
 
-          const customerIndex = newBarChartData.findIndex((item) => item['x'] === customerId);
+                    const customerIndex = newBarChartData.findIndex((item) => item['x'] === customerId);
 
-          if (customerIndex === -1) {
-            newBarChartData.push({
-              x: customerId,
-              y: revenue,
-            });
-          } else {
-            newBarChartData[customerIndex]['y'] += revenue;
-          }
-        });
+                    if (customerIndex === -1) {
+                        newBarChartData.push({
+                            x: customerId,
+                            y: revenue,
+                        });
+                    } else {
+                        newBarChartData[customerIndex]['y'] += revenue;
+                    }
+                });
 
-        newBarChartData.sort((a, b) => b['y'] - a['y']);
+                newBarChartData.sort((a, b) => b['y'] - a['y']);
 
-        barCustomSeries[0]['dataSource'] = newBarChartData;
+                barCustomSeries[0]['dataSource'] = newBarChartData;
 
-        //barPrimaryYAxis.maximum = totalRevenue;
-        //barPrimaryYAxis.interval = totalRevenue / 5;
+                //barPrimaryYAxis.maximum = totalRevenue;
+                //barPrimaryYAxis.interval = totalRevenue / 5;
 
-        setChartKey(prevKey => prevKey + 1);
+                setChartKey(prevKey => prevKey + 1);
 
-        var titleElement = document.getElementById('top10CustomersTitle');
-        titleElement.innerHTML = 'Top 10 Customers Sales';
+                var titleElement = document.getElementById('top10CustomersTitle');
+                titleElement.innerHTML = 'Top 10 Customers Sales';
 
-        var highestRevenue = newBarChartData[0]['y'];
-        var lowestRevenue = newBarChartData[0]['y'];
-        
+                var highestRevenue = newBarChartData[0]['y'];
+                var lowestRevenue = newBarChartData[0]['y'];
 
-        newBarChartData.forEach((item) => {
-          if (item['y'] > highestRevenue) {
-            highestRevenue = item['y'];
-          }
 
-          if (item['y'] < lowestRevenue) {
-            lowestRevenue = item['y'];
-          }
-        });
+                newBarChartData.forEach((item) => {
+                    if (item['y'] > highestRevenue) {
+                        highestRevenue = item['y'];
+                    }
 
-        var highestRevenueElement = document.getElementById('topCustomerSales');
-        highestRevenueElement.innerHTML = 'RM ' + highestRevenue.toString();
+                    if (item['y'] < lowestRevenue) {
+                        lowestRevenue = item['y'];
+                    }
+                });
 
-        var lowestRevenueElement = document.getElementById('lowestCustomerSales');
-        lowestRevenueElement.innerHTML = 'RM ' + lowestRevenue.toString();
+                var highestRevenueElement = document.getElementById('topCustomerSales');
+                highestRevenueElement.innerHTML = 'RM ' + highestRevenue.toString();
 
-      }
-    };
+                var lowestRevenueElement = document.getElementById('lowestCustomerSales');
+                lowestRevenueElement.innerHTML = 'RM ' + lowestRevenue.toString();
 
-    fetchBarData();
-  }, []);
+            }
+        };
 
-  return (
-    <ChartComponent
-      id="barTop10Customers"
-      key={chartKey}
-      primaryXAxis={barPrimaryXAxis}
-      primaryYAxis={barPrimaryYAxis}
-      width={width}
-      height={height}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      legendSettings={{ background: 'white' }}
-    >
-      <Inject services={[ColumnSeries, Category, Legend, Tooltip]} />
-      <SeriesCollectionDirective>
-        {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  );
+        fetchBarData();
+    }, []);
+
+    return (
+        <ChartComponent
+            id="barTop10Customers"
+            key={chartKey}
+            primaryXAxis={barPrimaryXAxis}
+            primaryYAxis={barPrimaryYAxis}
+            width={width}
+            height={height}
+            chartArea={{ border: { width: 0 } }}
+            tooltip={{ enable: true }}
+            background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+            legendSettings={{ background: 'white' }}
+        >
+            <Inject services={[ColumnSeries, Category, Legend, Tooltip]} />
+            <SeriesCollectionDirective>
+                {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+            </SeriesCollectionDirective>
+        </ChartComponent>
+    );
 };
 
 export default BarTop10Customers;
