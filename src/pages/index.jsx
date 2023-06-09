88f059d0b6d793818703005819baad9b8b064792
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-09 13:50:29 +0800

    Update App.js to include routing of pages only

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,7 +23,6 @@ export { default as Statistic } from './Statistic';
 export { default as Task } from './Task';
 export { default as Login } from './Login';
 export { default as SignUp } from './SignUp';
-export { default as Homepage } from './Homepage';
 export { default as History } from './History';
 export { default as FormReviewPage } from './FormReviewPage';
 export { default as FormReviewPosition } from './FormReviewPosition';
