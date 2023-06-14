0f94d213a08707a43f72de59a30a7a000bf7ff35
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-14 12:00:32 +0800

    Update History.jsx

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://aehwgrirrnhmatqmqcsa.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaHdncmlycm5obWF0cW1xY3NhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDg2NTg4MywiZXhwIjoxOTk2NDQxODgzfQ.DeXxoWY65kzpbvdxME16mAHj2KGMwDRg_jEGgUIxKc0';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from '../client';
 
 const History = () => {
   const [formData, setFormData] = useState([]);
