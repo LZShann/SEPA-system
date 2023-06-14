39afb0b89f7901bec0985ca54ee8e9701ea52643
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-14 11:59:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Statistic, Import_Dataset, Interview, Task, Account, Login, SignUp, FormReviewPage, FormReviewPosition, History } from './pages';
 import './App.css';
 
-import { useStateContext } from './contexts/ContextProvider';
-
-
 const App = () => {
 
   return (
@@ -14,15 +11,16 @@ const App = () => {
           <div>
             <Routes>
               {/* login & signup  */}
-              <Route index element={(<Login />)} />
-              <Route path="/SignUp" element={(<SignUp />)} />
+              <Route index element={<Login />} />
+              <Route path="/SignUp" element={<SignUp />} />
 
               {/* pages */}
-              <Route path="/Statistic" element={(<Statistic />)} />
-              <Route path="/Task" element={(<Task />)} />
-              <Route path="/Import_Dataset" element={(<Import_Dataset />)} />
+              <Route path="/Statistic" element={<Statistic />} />
+              <Route path="/Task" element={<Task />} />
+              <Route path="/Import_Dataset" element={<Import_Dataset />} />
               <Route path="/Interview" element={ <div> <Interview /> <History/> </div> } />
-              <Route path="/Account" element={(<Account />)} />
+              <Route path="/Account" element={<Account />} />
+
               {/* Interview pop up pdf */}
               <Route path="/form-review-page/:id" element={<FormReviewPage />} />
               <Route path="/form-review-position/:id" element={<FormReviewPosition />} />
