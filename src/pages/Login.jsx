f9e92b97d6b72035f7b972ec185bf1841fbf627b
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-14 12:00:35 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,21 +3,14 @@ import '../App.css';
 import logo from '../hcgm.png';
 import { supabase } from "../client";
 import { useNavigate, Link } from "react-router-dom";
+import { Sidebar, UserProfile } from '../components';
+import Statistic from './Statistic';
 
 const Login = () => {
 
     const navigate = useNavigate();
     const [formData, setFormData] = useState({email:'', password:''});
-    console.log(formData);
-
-    // supabase.auth.onAuthStateChange(async (event) => {
-    //     if (event === 'SIGNED_IN'){
-    //         navigate('/Success');
-    //     }
-    //     else {
-    //         navigate('/')
-    //     }
-    // })
+    const [userData, setUserData] = useState({id:'', role:''});
 
     async function handleChange(event){
       setFormData((prevFormData)=>{
@@ -28,42 +21,69 @@ const Login = () => {
       })
     };
 
+    
+
     async function handleSubmit(e){
       e.preventDefault()
       try {
+           // Sign in user from to Statistic based on email and password
           const { data, error } = await supabase.auth.signInWithPassword({
               email: formData.email,
               password: formData.password,
           })
+
+           // Fetch the user role from the database
+          const { data: roles, error: roleError } = await supabase
+            .from('users_data')
+            .select('role')
+            .eq('email', formData.email)
+            .single();
+          const userRole = roles ? roles.role : null;
+          setUserData(userRole);
+
+          // Fetch the user id from the database
+          const { data: id, error: idError } = await supabase
+            .from('users_data')
+            .select('id')
+            .eq('email', formData.email)
+            .single();
+          const userID = id ? id.id : null;
+          setUserData(userID);
           
           if (error) throw error
+          if (roleError) throw roleError
+          if (idError) throw idError
+          
           console.log(data)
+          console.log(roles)
+          console.log(id)
+          // sessionStorage.setItem('userRole', userRole)
+          // sessionStorage.setItem('userID', userID)
+          // console.log(sessionStorage)
           navigate('/Statistic')
-  
+
       } catch (error) {
          alert(error)
       }
     };
-
+    
   return (
     <div className='App'>
-        <header className='App-header'>
-            <div className="container-bg">
+            <div className="container-bg"> 
               <div className='container'>
                 <img className='logo' src={logo} />
                 <div className='container-box'>
                   <form onSubmit={handleSubmit}>
                     <p className='p-blue'>Your Email</p>
                     <input type="blue" name='email' placeholder="Email" required onChange={handleChange}/>
-                    <p className='p-red'>Your Password</p>
+                    <p className='p-blue'>Your Password</p>
                     <input type="password" name='password' placeholder="Password" required onChange={handleChange}/>
                     <input type="submit" value="Sign In"/>
                   </form>
                   <p>Don't have an account? Sign Up <Link to='/Signup'><u><b>Here</b></u></Link></p>
                 </div>
               </div>
-            </div>     
-        </header>
+            </div>   
     </div>
   );
 }
