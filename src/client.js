ebec9caaaae563b4c5cbcc7818b2351a3822f854
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-14 11:59:36 +0800

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://aehwgrirrnhmatqmqcsa.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaHdncmlycm5obWF0cW1xY3NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA4NjU4ODMsImV4cCI6MTk5NjQ0MTg4M30.njT-Q7wJu49HeqJTvFdPahtkYQm2QjVvfnhqEHmI7jE'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
