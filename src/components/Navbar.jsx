6d70dfb1d5eea8c62114c3b9a57f4f215950b4b4
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-15 15:56:17 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import avatar from '../data/profileicon.jpg';
 
-import { Notification, UserProfile } from '.';
+import { UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 import { supabase } from "../client";
 
@@ -15,7 +15,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       type="button"
       onClick={() => customFunc()}
       style={{ color }}
-      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+      className="relative text-xl rounded-full p-3 hover:bg-green-100"
     >
       <span
         style={{ background: dotColor }}
@@ -31,22 +31,24 @@ const Navbar = () => {
   //
   const [ formData, setFormData ] = useState([]);
 
-  const userID = sessionStorage.getItem('userID');
-  console.log('I\'m Navbar: ', userID);
+  const userName = sessionStorage.getItem('currentUserName');
 
-  async function retrieveName() {
-    const { data, error } = await supabase
-    .from('users_data')
-    .select('*')
-    .eq('id', userID)
+  // async function retrieveName() {
+  //   const { data, error } = await supabase.auth.refreshSession()
+  //   const { session, user } = data;
 
-    if (error) {
-      console.error('Error retrieving data:', error);
-    } else {
-      console.log('Retrieved data:', data);
-      setFormData(data);
-    }
-  }
+  //   const { data: user_data, error: user_error } = await supabase
+  //     .from('users_data')
+  //     .select('*')
+  //     .eq('uuid', user.id);
+
+  //   if (user_error) {
+  //     console.error('Error retrieving data:', user_error);
+  //   } else {
+  //     console.log('Retrieved data:', user_data);
+  //     setFormData(user_data);
+  //   }
+  // };
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -56,7 +58,8 @@ const Navbar = () => {
     handleResize();
 
     return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    
+  });
 
   useEffect(() => {
     if (screenSize <= 900) {
@@ -66,20 +69,42 @@ const Navbar = () => {
     }
   }, [screenSize]);
 
-  useEffect(() => {
-    retrieveName();
-  }, []);
+  // useEffect(() => {
+  //   retrieveName();
+  // }, []);
+
+  // useEffect(() => {
+  //   supabase.auth.getSession().then((data, error) => {
+  //     // Check if session is found
+  //     if (data['data']['session'] == null) {
+  //       // User not logged in
+  //       window.location.replace('/');
+  //     }
+  //     else {
+  //       const userAuthData = data['data']['session'];
+        
+  //       // Fetch user role
+  //       supabase
+  //         .from('users_data')
+  //         .select('*')
+  //         .eq('uuid', userAuthData['user']['id'])
+  //         .then((data, error) => {
+  //           setUserName(data['data'][0]['name']);
+  //         });
+  //     }
+  //   });
+  // });
 
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+    <div className="flex justify-between p-2 relative bg-main-bg">
 
       <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
         <TooltipComponent content="Profile" position="BottomCenter">
           <div
-            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-green-100 rounded-lg"
             onClick={() => handleClick('userProfile')}
           >
             <img
@@ -89,17 +114,14 @@ const Navbar = () => {
             />
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{' '}
-                {formData.map((item) => (
-                  <span key={item.id} className="text-gray-400 font-bold ml-1 text-14">
-                    {item.name}
-                  </span>
-                ))}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                {userName}
+              </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
 
-        {isClicked.notification && (<Notification />)}
         {isClicked.userProfile && (<UserProfile />)}
       </div>
     </div>
