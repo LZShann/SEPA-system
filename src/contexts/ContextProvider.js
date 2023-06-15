9f579abc44d7e25ebeccb358afb20930b0828cbd
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-15 15:56:59 +0800

    Changed default colour

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,7 +10,7 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(undefined);
-  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentColor, setCurrentColor] = useState('#269529');
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
 
