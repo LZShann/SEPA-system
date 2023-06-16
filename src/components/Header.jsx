0f151e676d1f15d5698dd3f18e0fd408621ef2ab
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-16 23:27:24 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Header = ({ category, title }) => (
-  <div className="m-2 md:m-2 p-2 md:p-2">
-    <p className="text-lg text-gray-400">{category}</p>
-    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+  <div className="m-2 md:m-10 mt-5 md:mt-10 p-3 md:p-6">
+    <p className="text-2xl text-gray-400">{category}</p>
+    <p className="text-5xl font-extrabold tracking-tight text-slate-800">
       {title}
     </p>
   </div>
