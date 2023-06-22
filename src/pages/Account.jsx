43f20a6729d74d9b0ac8c508a1020b2b981f78cb
LZShann <92285116+LZShann@users.noreply.github.com>
2023-06-23 02:02:46 +0800

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -10,12 +10,13 @@ const Employees = () => {
 
   const currentUserSession = sessionStorage.getItem('currentUserSession');
 
-  const navigate = useNavigate();
-
   const [formData, setFormData] = useState({name:'', phone:'', email:'', password:'', role:'', department:'', supervisor:''});
   const [showAddUserModal, setShowAddUserModal] = useState(false);
   const [showEditUserModal, setShowEditUserModal] = useState(false);
+  const [showDeleteUserModal, setShowDeleteUserModal] = useState(false);
   const [employees, setEmployees] = useState([]);
+  const [editFormData, setEditFormData] = useState({name:'', phone:'', role:'', department:'', supervisor:''});
+  const [deleteData, setDeleteData] = useState({id:''});
 
   const {activeMenu} = useStateContext();
 
@@ -42,76 +43,187 @@ const Employees = () => {
       }
   };
 
-  ///////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////
   //Add Users
   function openAddUserModal() {
     setShowAddUserModal(true);
   };
 
   function closeAddUserModal() {
-    if (currentUserSession)
     setShowAddUserModal(false);
   };
+
+  const handleAddSubmit = (event) => {
+    event.preventDefault();
+    addEmployees();
+  };
   
-  async function handleAddEmployees(e) {
-    // e.preventDefault();
-    // try {
-    //   const { data, error } = await supabase.auth.signUp({
-    //     email: formData.email,
-    //     password: formData.password,
-    //     options: {
-    //       data: {
-    //         name: formData.name,
-    //         phone: formData.phone,
-    //         role: formData.role,
-    //         department: formData.department,
-    //         supervisor: formData.supervisor
-    //       }
-    //     }
-    //   });
+  const addEmployees = async () => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        options: {
+          data: {
+            name: formData.name,
+            phone: formData.phone,
+            role: formData.role,
+            department: formData.department,
+            supervisor: formData.supervisor
+          }
+        }
+      });
 
-    //   if (error){
-    //     console.log('Error inserting data:', error.message);
-    //   }
+      if (error){
+        console.log('Error inserting data:', error.message);
+      }
 
-    //   alert('Sign Up Success. Please verify your email.');
-    //   console.log('Data inserted successfully');
+      alert('Add User Success. Please verify their email. You will be directed to the login page.');
+      console.log('Data inserted successfully');
 
-    //   if (supabase.auth.getSession(data['session: access_token']) == currentUserSession)
-    //   navigate('/Account');
-    //   closeAddUserModal();
+      closeAddUserModal();
       
-    // } catch (error) {
-    //   alert(error.message);
-    // }
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
-  //////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
   //Edit Users
 
  //Edit Users
-  const openEditUserModal = () => {
+  const openEditUserModal = async (selectedUserID) => {
+
+    const {data, error} = await supabase
+    .from('users_data')
+    .select('*')
+    .eq('id', selectedUserID);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const fetchedUser = data[0];
+    console.log(fetchedUser);
+
+    setEditFormData({
+      ...fetchedUser
+    });
+
+    // setFormData((prevFormData)=>{
+    //   return{
+    //     ...prevFormData,
+    //     [event.target.name]:event.target.value
+    //   }
+    // })
+
     setShowEditUserModal(true);
   };
 
-  async function editEmployees() {
-    
+  const handleEditSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    // Call the editTask function with the task ID and editTaskData
+    editEmployees(editFormData.id, editFormData);
+  };
+  
+  const handleEditInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
   };
 
-  ////////////////////////////////////////////////////
+const editEmployees = async (selectedUserID, updatedUserData) => {
+  try {
+    // Update the task in the 'tasks' table using the Supabase client
+    const { data, error } = await supabase
+      .from('users_data')
+      .update(updatedUserData)
+      .match({ id: selectedUserID });
+
+    if (error) {
+      // Handle the error or provide feedback to the user
+      console.error(error);
+      return;
+    }
+
+    alert('Edit User Success.')
+    fetchEmployees()
+    closeEditUserModal();
+
+  } catch (error) {
+    // Handle any other errors that may occur
+    console.error(error);
+  }
+};
+
+  function closeEditUserModal() {
+    setShowEditUserModal(false);
+  };
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   //Delete Users
-  async function deleteEmployees(selectedUserID) {
+
+  const handleDeleteSubmit = (event) => {
+    event.preventDefault();
+    deleteEmployees(deleteData.id);
+  };
+
+  const openDeleteUserModal = async (selectedUserID) => {
+
+    const {data, error} = await supabase
+    .from('users_data')
+    .select('*')
+    .eq('id', selectedUserID);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const fetchedUserID = data[0];
+    console.log(fetchedUserID);
+
+    setDeleteData({
+      ...fetchedUserID
+    });
+
+    setShowDeleteUserModal(true);
+  };
+
+  const closeDeleteUserModal = () => {
+    setShowDeleteUserModal(false);
+  };
+
+  const deleteEmployees = async (selectedUserID) => {
+    try {
       const {data, error} = await supabase
       .from('users_data')
       .delete()
-      .eq('id', selectedUserID);
+      .match({id: selectedUserID});
+
+      if (error) {
+        // Handle the error or provide feedback to the user
+        console.error(error);
+        return;
+      }
+
+      alert('Delete User Success.')
       fetchEmployees();
+      closeDeleteUserModal();
+    } catch (error) {
+      // Handle any other errors that may occur
+      console.error(error);
+    }
   };
 
   //UseEffect
   useEffect(() => {
     fetchEmployees();
-    // editEmployees();
+    editEmployees();
     deleteEmployees();
   }, []);
 
@@ -171,8 +283,8 @@ const Employees = () => {
                       <td>{user.department}</td>
                       <td>{user.supervisor}</td>
                       <td>
-                        <button className="EditButton" onClick={() => openEditUserModal()}>Edit</button>
-                        <button className="DeleteButton" onClick={() => deleteEmployees(user.id)}>Delete</button>
+                        <button className="EditButton" onClick={() => openEditUserModal(user.id)}>Edit</button>
+                        <button className="DeleteButton" onClick={() => openDeleteUserModal(user.id)}>Delete</button>
                       </td>
                     </tr>
                   ))}
@@ -182,7 +294,7 @@ const Employees = () => {
 
           {/* Add User */}
           {showAddUserModal && (
-            <form onSubmit={handleAddEmployees}>
+            <form onSubmit={addEmployees}>
                 <div className="fixed inset-0 flex items-center justify-center z-50">
                   <div className="bg-white rounded-lg shadow-lg p-6 w-96">
                     <h2 className="text-xl font-semibold mb-4">Add New User</h2>
@@ -275,8 +387,10 @@ const Employees = () => {
                         <option value="Sales">Sales</option>
                         <option value="HR">HR</option>
                         <option value="Admin">Admin</option>
-                        <option value="Account">Account</option>
+                        <option value="Account">Account&Finance</option>
                         <option value="Production">Production</option>
+                        <option value="Management">Management</option>
+                        <option value="Marketing">Marketing</option>
                       </select>
                     </div>
                     <div className="mb-4">
@@ -300,6 +414,7 @@ const Employees = () => {
 
                     <div className="flex justify-end">
                       <button
+                        onClick={handleAddSubmit}
                         className="px-4 py-2 text-white bg-indigo-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
                       >
                         Add
@@ -318,6 +433,7 @@ const Employees = () => {
 
           {/* Edit User */}
           {showEditUserModal && (
+            <form onSubmit={editEmployees}>
                 <div className="fixed inset-0 flex items-center justify-center z-50">
                   <div className="bg-white rounded-lg shadow-lg p-6 w-96">
                     <h2 className="text-xl font-semibold mb-4">Edit User</h2>
@@ -330,8 +446,8 @@ const Employees = () => {
                         name="name"
                         id="name"
                         className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        value={formData.name}
-                        onChange={handleChange}
+                        value={editFormData.name}
+                        onChange={handleEditInputChange}
                         required
                       />
                     </div>
@@ -344,8 +460,8 @@ const Employees = () => {
                         name="phone"
                         id="phone"
                         className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        value={formData.phone}
-                        onChange={handleChange}
+                        value={editFormData.phone}
+                        onChange={handleEditInputChange}
                         required
                       />
                     </div>
@@ -357,8 +473,8 @@ const Employees = () => {
                         name="role"
                         id="role"
                         className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        value={formData.role}
-                        onChange={handleChange}
+                        value={editFormData.role}
+                        onChange={handleEditInputChange}
                         required
                       >
                         <option value="">Select New Role</option>
@@ -374,16 +490,18 @@ const Employees = () => {
                         name="department"
                         id="department"
                         className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        value={formData.department}
-                        onChange={handleChange}
+                        value={editFormData.department}
+                        onChange={handleEditInputChange}
                         required
                       >
                         <option value="">Select New Department</option>
                         <option value="Sales">Sales</option>
                         <option value="HR">HR</option>
                         <option value="Admin">Admin</option>
-                        <option value="Account">Account</option>
+                        <option value="Account&Finance">Account</option>
                         <option value="Production">Production</option>
+                        <option value="Management">Management</option>
+                        <option value="Marketing">Marketing</option>
                       </select>
                     </div>
                     <div className="mb-4">
@@ -394,8 +512,8 @@ const Employees = () => {
                         name="supervisor"
                         id="supervisor"
                         className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        value={formData.supervisor}
-                        onChange={handleChange}
+                        value={editFormData.supervisor}
+                        onChange={handleEditInputChange}
                         required
                       >
                         <option value="">Select New Supervisor</option>
@@ -407,13 +525,13 @@ const Employees = () => {
 
                     <div className="flex justify-end">
                       <button
-                        onClick={editEmployees()}
+                        onClick={handleEditSubmit}
                         className="px-4 py-2 text-white bg-indigo-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
                       >
                         Save
                       </button>
                       <button
-                        onClick={() => setShowEditUserModal(false)}
+                        onClick={closeEditUserModal}
                         className="px-4 py-2 ml-2 text-gray-600 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
                       >
                         Cancel
@@ -422,8 +540,24 @@ const Employees = () => {
                     </div>
                   </div>
                 </div>
+            </form>
+          )}
 
-              )}
+          {/* Delete User */}
+          {showDeleteUserModal && (
+            <form onSubmit={handleDeleteSubmit}>
+              <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+                <h2 className="text-xl font-semibold mb-4">Delete User</h2>
+                    <p>Are you sure you want to delete this user?</p>
+                    <div className="mb-4">
+                      <button className="DeleteButton" onClick={deleteEmployees}>Delete</button>
+                      <button className="CancelButton" onClick={closeDeleteUserModal}>Cancel</button>
+                    </div>
+                </div>
+              </div>
+            </form>
+            )}
         </div>
       </div>
       <Footer />
